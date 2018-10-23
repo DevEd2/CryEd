6b863c523a6ee40b261fadd017ed68b301fb190a
unknown <deved8@gmail.com>
2018-10-23 10:06:43 -0400

    Adding files

---
+++ audio/music_pointers.asm
@@ -0,0 +1,10 @@
+; See song sections in audio.asm.
+; We don't need the entire soundtrack to Pokemon Crystal, just a few tracks...
+; Hopefully I can keep all the sound code in one bank
+
+Music: ; e906e
+; entries correspond to MUSIC_* constants
+
+	dba Music_Nothing
+	dba Music_ViridianCity
+	dba	Music_Credits
